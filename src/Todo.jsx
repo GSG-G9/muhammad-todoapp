e15975832af7089fe475260a36d03ae449802968
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-31 13:44:35 +0200

    Add sample todo and add todo to todolist Relates #9

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Todo.css';
 
 export default class Todo extends Component {
   render() {
     return (
-      <div>
-
+      <div className="Todo">
+        <li className="Todo-task">
+          <div className="Todo-task_type">
+            
+          </div>
+          <div className="Todo-task_content">{this.props.task}</div>
+        </li>
+        <div className="Todo-buttons">
+          <button><i className="fas fa-pen"></i></button>
+          <button><i className="fas fa-trash"></i></button>
+        </div>
       </div>
     )
   }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
+import "./TodoList.css";
 
 export default class TodoList extends Component {
   render() {
-    return <div>
-      <Todo />
-      <NewTodoForm />
-    </div>;
+    return (
+      <div className="TodoList">
+        <h1>
+          Todo List! <span>A Simple React Todo List App.</span>
+        </h1>
+        <ul>
+          <Todo task="New Todo" completed="false" />
+          <Todo task="New Todo" completed="false" />
+          <Todo task="New Todo" completed="false" />
+        </ul>
+        <NewTodoForm />
+      </div>
+    );
   }
 }
