0467f4162d5f971d8058e45449899ee6e157d9ba
Muhammad Alabadsa <moh22389@gmail.com>
2020-12-31 14:47:14 +0200

    Add Completed toggle and render todo with fake data Relates #10

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import './Todo.css';
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleToggle(){
+    console.log(this.props.completed);
+    console.log(this.props.id);
+    this.props.toggleTodo(this.props.id);
+  }
   render() {
     return (
       <div className="Todo">
-        <li className="Todo-task">
+        <li className="">
           <div className="Todo-task_type">
             
           </div>
-          <div className="Todo-task_content">{this.props.task}</div>
+          <div className={this.props.completed ? 'Todo-task completed' : 'Todo-task' } onClick={this.handleToggle}>{this.props.task}</div>
         </li>
         <div className="Todo-buttons">
           <button><i className="fas fa-pen"></i></button>
